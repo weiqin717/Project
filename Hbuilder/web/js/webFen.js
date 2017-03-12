c95bf9d0f667b3b3d6a853ed83b9f5c6e1190e7d
qinwei <qinwei@qq.com>
2017-03-12 18:34:46 +0800

    整理

--- Hbuilder/web/js/webFen.js
+++
@@ -1,5 +0,0 @@
-$(function(){
-  $('.other').click(function(){
-    $('.foot2').slideToggle('slow');
-  })
-})
--- Hbuilder/web/js/webset.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-  $('.setTwo').click(function(){
-    $('.one').css('display','none');
-    $('.preson').css('display','none');
-    var index = $(this).index();
-    $('.preson').eq(index).css('display','block');
-  })
-})
